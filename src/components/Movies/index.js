0ba97a797a264063ea1ebc3ad3f6efbb1df97cee
Nick Murphy <63930644+Murphyn5@users.noreply.github.com>
2023-01-31 16:48:45 -0800

    Initial commit

---
+++ src/components/Movies/index.js
@@ -0,0 +1,22 @@
+import { Route, Switch, NavLink } from "react-router-dom";
+import MovieDetails from "../MovieDetails/index.js";
+
+function Movies({ movies }) {
+  return (
+    <div className="comp orange">
+      <h1>Movies Component</h1>
+      <nav>
+        {movies.map((movie) => (
+          <NavLink to={`/movies/${movie.id}`}>{movie.title}</NavLink>
+        ))}
+      </nav>
+      <Switch>
+        <Route path="/movies/:movieId">
+          <MovieDetails movies={movies} />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default Movies;
